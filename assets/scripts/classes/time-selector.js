109e15f1fa98afab766d691f224ca0e0cef5f968
Chris Nelson <cnelson87@gmail.com>
2013-06-19 10:34:08 -0700

    edits

--- assets/scripts/classes/time-selector.js
+++ assets/scripts/classes/time-selector.js
@@ -1,30 +1,38 @@
 
 var TimeSelector = Backbone.View.extend({
-	//el: '#app',
-	tagName: 'fieldset',
-	className: 'time-selector',
-	//template: $('#tmpTimeSelector').html(),
+	el: '#time-selector',
+	//tagName: 'fieldset',
+	//className: 'time-selector',
+	template: $('#tmpTimeSelector').html(),
 	events: {
 		'change select': 'onSelectChange'
 	},
 
 	initialize: function(){
-		//console.log(this.el);
-		//console.log(this.$el);
+		console.log(this.el);
+		console.log(this.$el);
 		this.template = $('#tmpTimeSelector').html();
 		this.render();
-	},
-
-	getTime: function(){
-
-	},
-
-	setTime: function(){
-
+		this.$elHour = this.$el.find('.hour');
+		this.$elMinute = this.$el.find('.minute');
+		this.$elAmpm = this.$el.find('.ampm');
+		//console.log(this.$elHour, this.$elMinute, this.$elAmpm);
 	},
 
 	onSelectChange: function(e){
+		this.getTime();
+	},
 
+	getTime: function(){
+		var hour = this.$elHour.val();
+		var minute = this.$elMinute.val();
+		var ampm = this.$elAmpm.val();
+		var time = hour + ':' + minute + ' ' + ampm;
+		console.log(time);
+	},
+
+	setTime: function(time){
+		console.log('setTime');
 	},
 
 	render: function(){
